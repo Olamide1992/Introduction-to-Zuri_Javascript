1a6ac21e868e9c71f3a1621fbbe01955ffde8929
Olamide1992 <mikaheelazeez192@gmail.com>
2022-06-21 10:07:41 -0700

    modified

--- index.js
+++ index.js
@@ -2,11 +2,41 @@
 
 function my_details(name, height, country) {
     var name = "Mikaheel Azeez Babatunde"
-    var height = "56"
+    var height = "189cm"
     var country = 'Nigeria'
-     alert("Here are my full details:  My name is : " + name + ";      My height is: " + height + ";   and My country is: " + country)
+     alert("Here are my full details:  My name is :" + " " + name +  " ; " + "I am" + " " + height + " tall " + "; " + "and I am from a Country Known as" + " " + country)
 }
 my_details()
+
+// //Function to call my name;
+// function Myname(name) {
+//     var name = "Mikaheel Azeez Babatunde"
+//     alert ("Hello my name is" + " " + name)
+
+// }
+
+
+
+
+// //Function to call my height;
+// function MyHeight(height) {
+//     var height = "140cm"
+//     alert ("I am" + " " + height + " " + "tall" )
+
+// }
+
+// //Function to call my Country;
+// function MyCountry(country) {
+//     var Country = "Nigeria"
+//     alert (" And I am from the country called" + " " + Country)
+
+// }
+
+
+// Myname()
+// MyHeight()
+// MyCountry()
+
 
 
 
