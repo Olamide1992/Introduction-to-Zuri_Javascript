31e52191b019f32dbb0393038fcb792bcee49330
Olamide1992 <mikaheelazeez192@gmail.com>
2022-06-20 23:51:14 -0700

    adding file

---
+++ index.js
@@ -0,0 +1,14 @@
+//Write a javascript code that prints out your name, height, and country on the screen.
+
+function my_details(name, height, country) {
+    var name = "Mikaheel Azeez Babatunde"
+    var height = "56"
+    var country = 'Nigeria'
+     alert("Here are my full details:  My name is : " + name + ";      My height is: " + height + ";   and My country is: " + country)
+}
+my_details()
+
+
+
+
+
